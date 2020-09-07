6fcbc6d79cc1a41d1abc71665332451a12eebf04
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-09-07 02:17:31 -0500

    Set up core plugin framework

--- CustomEvents.BeatSaber/CEPlugin.cs
+++ CustomEvents.BeatSaber/CEPlugin.cs
@@ -1,9 +1,12 @@
 ﻿using HarmonyLib;
 using IPA;
+using IPA.Loader;
 using IPA.Logging;
+using SiraUtil.Zenject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,11 +20,13 @@ namespace CustomEvents
 
         public Logger Log { get; }
         public Harmony Harmony { get; }
+        public PluginMetadata Metadata { get; }
 
         [Init]
-        public CEPlugin(Logger log)
+        public CEPlugin(Logger log, PluginMetadata meta)
         {
             instance = this;
+            Metadata = meta;
             Log = log;
             Harmony = new Harmony("com.cirr.danike.CustomEvents");
         }
@@ -29,13 +34,17 @@ namespace CustomEvents
         [OnEnable]
         public void OnEnable()
         {
-
+            Log.Debug($"Enabling...");
+            Harmony.PatchAll(Assembly.GetCallingAssembly());
+            Installer.RegisterAppInstaller<PluginInstaller>();
+            Log.Debug($"Enabled {Metadata.Name} version {Metadata.Version}");
         }
 
         [OnDisable]
         public void OnDisable()
         {
-
+            Installer.UnregisterAppInstaller<PluginInstaller>();
+            Harmony.UnpatchAll(Harmony.Id);
         }
     }
 }
---
+++ CustomEvents.BeatSaber/PluginInstaller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace CustomEvents
+{
+    internal class PluginInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInstance(CEPlugin.Instance);
+        }
+    }
+}
