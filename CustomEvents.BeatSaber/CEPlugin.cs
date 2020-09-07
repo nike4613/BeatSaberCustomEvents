d6c1afcd6c93451f3224018d591cdcab2d0e6ff6
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-09-07 02:07:48 -0500

    Added core plugin stuff

---
+++ CustomEvents.BeatSaber/CEPlugin.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using IPA;
+using IPA.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEvents
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    internal class CEPlugin
+    {
+        private static CEPlugin? instance;
+        public static CEPlugin Instance => instance ?? throw new InvalidOperationException();
+
+        public Logger Log { get; }
+        public Harmony Harmony { get; }
+
+        [Init]
+        public CEPlugin(Logger log)
+        {
+            instance = this;
+            Log = log;
+            Harmony = new Harmony("com.cirr.danike.CustomEvents");
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+
+        }
+    }
+}
---
+++ CustomEvents.BeatSaber/description.md
@@ -0,0 +1,1 @@
+﻿TODO: fill this out
